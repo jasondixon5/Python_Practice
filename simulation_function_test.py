8bee1e0bd061e3ebe24701318d0075cb2a74eb8a
Jason Anthony Dixon <jasondixonmail@gmail.com>
2016-07-29 11:26:48 -0700

    updated simulation function

--- simulation_function_test.py
+++ simulation_function_test.py
@@ -6,6 +6,6 @@ male_observed = [21, 3]
 female_observed = [14, 10]
 all_results = [male_observed, female_observed]
 
-m2f_diff = sf.diff_proportion(all_results)
+m2f_diff_results = sf.diff_proportion(all_results)
 
-print(m2f_diff)
+print(m2f_diff_results)
--- simulation_functions.py
+++ simulation_functions.py
@@ -39,6 +39,50 @@ def diff_proportion(observations, cat1_column=0, cat2_column=1):
     proportion_R2C1 = observations[r_cat2_idx][c_cat1_idx] / r_cat2_n
     
     proportion_difference = proportion_R1C1 - proportion_R2C1
+    
+    #Form list of just observed values in column category 1
+    c_cat1_obs = []
 
-    return (proportion_difference, r_cat1_n, r_cat2_n, proportion_R1C1, proportion_R2C1)
+    for i in range(len(observations)):
+        c_cat1_obs.append(observations[i][c_cat1_idx])
+    
+    simulated_cat1_n = sum(c_cat1_obs)
+    
+    large_sim_diff_rate_R1toR2 = []
+    
+    sim_size = 10000
+    
+    for _ in range(sim_size):
+        simulated_results_C1R1 = 0
+        simulated_results_C1R2 = 0
+    
+        for _ in range(simulated_cat1_n):
+            if random.random() < 0.5:
+                simulated_results_C1R2 += 1
+            else:
+                simulated_results_C1R1 += 1
+        
+        simulated_rate_R2C1 = simulated_results_C1R2 / simulated_cat1_n
+    
+        simulated_rate_R1C1 = simulated_results_C1R1 / simulated_cat1_n
+    
+        diff_in_simulated_rate_R1toR2 = simulated_rate_R1C1 - simulated_rate_R2C1
+        
+        large_sim_diff_rate_R1toR2.append(diff_in_simulated_rate_R1toR2)
+
+    count_diffs_as_large = 0
+    
+    for i in large_sim_diff_rate_R1toR2:
+        if i >= proportion_difference:
+            count_diffs_as_large += 1
+    
+    p_value_proportion_difference = count_diffs_as_large / sim_size
+    
+    return ("""
+    proportion difference: {}, 
+    proportion R1C1: {}, 
+    proportion R2C1: {}, 
+    p-value: {}
+    sim_size: {}""".format(proportion_difference, proportion_R1C1, proportion_R2C1, p_value_proportion_difference, sim_size))
+    
 
